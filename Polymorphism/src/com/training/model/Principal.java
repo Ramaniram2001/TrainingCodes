31267dd98dd728daf7d6f02efc6e442794b383a2
Haripriya.R <Haripriya.R@EXIND3010-76.allfon.in>
2022-04-07 12:26:46 +0530

    Forth commit

---
+++ Polymorphism/src/com/training/model/Principal.java
@@ -0,0 +1,49 @@
+package com.training.model;
+
+import com.training.ifaces.Conditional;
+
+public class Principal implements Conditional {
+	private String typeOfCollege;
+	
+	
+
+	public Principal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+
+	public Principal(String typeOfCollege) {
+		super();
+		this.typeOfCollege = typeOfCollege;
+	}
+
+
+
+
+	public String getTypeOfCollege() {
+		return typeOfCollege;
+	}
+
+
+
+
+	public void setTypeOfCollege(String typeOfCollege) {
+		this.typeOfCollege = typeOfCollege;
+	}
+
+
+
+
+	@Override
+	public boolean test(Object value) {
+		String typeOfCollege=(String)value;
+		boolean result=false;
+		if(typeOfCollege.equals("engg")) {
+		return true;
+	}
+		return result;
+	}
+}
